220e78d799cfec4012e59828b2cdd34cd7841c31
dev <dev@local>
2026-01-18 11:40:25 +0300

    feat(app): expose TreasuryKeeper field for e2e tests

--- app/app.go
+++ app/app.go
@@ -46,6 +46,7 @@ import (
 	ibckeeper "github.com/cosmos/ibc-go/v10/modules/core/keeper"
 
 	"github.com/tmp/marketplace/docs"
+	treasurymodulekeeper "github.com/tmp/marketplace/x/treasury/keeper"
 )
 
 const (
@@ -98,7 +99,8 @@ type App struct {
 	TransferKeeper      ibctransferkeeper.Keeper
 
 	// simulation manager
-	sm *module.SimulationManager
+	sm             *module.SimulationManager
+	TreasuryKeeper *treasurymodulekeeper.Keeper
 }
 
 func init() {
